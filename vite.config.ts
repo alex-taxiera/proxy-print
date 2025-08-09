136182abf8d928e4acb92c10b91e61c62bda7bb1
Alex Taxiera <alex.taxiera@gmail.com>
2025-08-09 06:11:52 +0000

    add sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "proxy-print",
+      project: "proxy-print",
+    }),
+  ],
   worker: {
     format: 'es',
   },
-})
+  build: {
+    sourcemap: true,
+  },
+});
