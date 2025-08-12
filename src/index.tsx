edf9056f0ca30729ed96455a3e1adeb192bef85a
Alex Taxiera <alex.taxiera@gmail.com>
2025-08-12 18:26:06 +0000

    enable a tiny amount of sessions

--- src/index.tsx
+++ src/index.tsx
@@ -28,7 +28,7 @@ Sentry.init({
     Sentry.consoleLoggingIntegration({ levels: ["log", "error", "warn"] }),
   ],
 
-  replaysSessionSampleRate: 0,
+  replaysSessionSampleRate: 0.01,
   replaysOnErrorSampleRate: 1,
 
   tracesSampleRate: SENTRY_ENV === "production" ? 0.1 : 1,
