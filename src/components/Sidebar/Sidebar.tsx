037f5ffd0cad2e11c9eb6d81d8bb200e80e53f18
Alex Taxiera <alex.taxiera@gmail.com>
2025-08-14 18:37:27 +0000

    fix sidebar scrollbars in firefox

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -54,6 +54,7 @@ export const Sidebar = () => {
           className={css({
             height: "full",
             overflowY: "auto",
+            overflowX: "hidden",
             paddingBottom: "12", // dont let edge go to bottom of page
             paddingLeft: "1",
           })}
