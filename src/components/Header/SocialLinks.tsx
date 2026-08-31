4c1d57571de5dc5c17b20f03adb045abd62b5c7a
Alex Taxiera <alex.taxiera@gmail.com>
2026-08-31 19:25:07 +0000

    feat(header): new header including card anvil link

--- src/components/Header/SocialLinks.tsx
+++ src/components/Header/SocialLinks.tsx
@@ -1,9 +1,24 @@
-import { ButtonGroup, IconButton } from "@chakra-ui/react";
+import { ButtonGroup, IconButton, Button, Icon } from "@chakra-ui/react";
 import { FaGithub, FaDiscord, FaPaypal } from "react-icons/fa";
 
 export const SocialLinks = () => {
   return (
-    <ButtonGroup gap="0" color="fg" variant="ghost">
+    <ButtonGroup
+      color="fg"
+      size={{ base: "2xs", sm: "xs" }}
+      gap={{ base: "2", sm: "4" }}
+      variant="ghost"
+      colorPalette="accent"
+    >
+      <Button variant="outline" asChild>
+        <a
+          href="https://cardanvil.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Create MTG cards!
+        </a>
+      </Button>
       <IconButton
         asChild
         aria-label="View the Proxy Print Setup GitHub repository"
@@ -13,29 +28,31 @@ export const SocialLinks = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FaGithub />
+          <Icon size={{ base: "sm", sm: "md" }}>
+            <FaGithub />
+          </Icon>
         </a>
       </IconButton>
-      <IconButton
-        asChild
-        color="inherit"
-        aria-label="Join the Proxy Community Discord"
-      >
+      <IconButton asChild aria-label="Join the Proxy Community Discord">
         <a
           href="https://discord.gg/A5AkkyP8CU"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FaDiscord />
+          <Icon size={{ base: "sm", sm: "md" }}>
+            <FaDiscord />
+          </Icon>
         </a>
       </IconButton>
-      <IconButton asChild color="inherit" aria-label="Donate via PayPal">
+      <IconButton asChild aria-label="Donate via PayPal">
         <a
           href="https://www.paypal.me/alextaxiera"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FaPaypal />
+          <Icon size={{ base: "sm", sm: "md" }}>
+            <FaPaypal />
+          </Icon>
         </a>
       </IconButton>
     </ButtonGroup>
